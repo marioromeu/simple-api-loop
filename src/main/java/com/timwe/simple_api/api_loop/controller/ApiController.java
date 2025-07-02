d81e4f1f27804b34c7d333947bbbe3f498905933
marioromeu <marioromeu@Aldebaran>
2025-07-02 14:54:31 -0300

    k

--- src/main/java/com/timwe/simple_api/api_loop/controller/ApiController.java
+++ src/main/java/com/timwe/simple_api/api_loop/controller/ApiController.java
@@ -1,56 +1,21 @@
 package com.timwe.simple_api.api_loop.controller;
 
 import java.io.IOException;
-import java.util.concurrent.TimeoutException;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.timwe.simple_api.api_loop.service.ApiService;
-import com.timwe.simple_api.api_loop.service.RabbitMQConsumerService;
-import com.timwe.simple_api.api_loop.service.RabbitMQProducerService;
 
 @RestController
 public class ApiController {
 
     private final ApiService apiService;
-    
-    private final RabbitMQConsumerService consumerService;
-    
-    private final RabbitMQProducerService producerService;
 
     public ApiController(
-    		ApiService apiService, 
-    		RabbitMQConsumerService consumerService,
-    		RabbitMQProducerService producerService) throws IOException {
-        this.apiService = apiService;
-        this.consumerService = consumerService;
-        this.producerService = producerService;
-        
-   		consumerService.startConsumer();
-        System.out.println("Consumidor iniciado");        
-        
-    }
-
-    @GetMapping("/start-consumer")
-    public String startConsumer() throws IOException {
-   		consumerService.startConsumer();
-        return "Consumidor iniciado";
-    }
-    
-    @GetMapping("/produce/{count}")
-    public String produce(@PathVariable int count) {
-    	try {
-    		producerService.produce(count);
-		} catch (IOException e) {
-			e.printStackTrace();
-			return "Message failed";			
-		} catch (TimeoutException e) {
-			e.printStackTrace();
-			return "Message failed";			
-		}
-        return "Message produced";
+    		ApiService apiService) throws IOException {
+        this.apiService = apiService;        
     }
 
     @GetMapping("/execute/{repetitions}")
